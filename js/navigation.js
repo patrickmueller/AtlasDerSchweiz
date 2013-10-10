6be445321e06ac9fe2e8c3e1792d336fd9892612
Patrick Müller <patrick@pmvm.net>
2013-10-10 11:46:42 +0200

    Überarbeitetes Menüverhalten

--- js/navigation.js
+++ js/navigation.js
@@ -1,20 +1,52 @@
 $(document).ready(function() {
 
-	$('.button').click(function() {
+	$('.button').click(menuControl);
+	$('#map_canvas').click(function() {
+		if($('.active').length > 0) menuControl();
+	});
+
+});
+
+function menuControl() {
+
+	if($(this).attr('id') == 'map_canvas') {
+		closeMenu();
+	} else {
 
 		var menuId = $(this).attr('data-menu-id');
 		var groupId = $(this).parent().parent().attr('id');
 		var menuAlignment = $(this).parent().parent().attr('data-alignment');
 
-		if(menuAlignment == 'left') {
-			$('#' + menuId).toggleClass('menu-open-left');
-			$('#' + groupId).toggleClass('button-open-left');
+		if($(this).hasClass('active')) {
+			$('.active').removeClass('active');
+			closeMenu();
 		} else {
-			$('#' + menuId).toggleClass('menu-open-right');
-			$('#' + groupId).toggleClass('button-open-right');
+			closeMenu();
+			$('.active').removeClass('active');
+			$(this).addClass('active');
+			openMenu();
 		}
 
-		return false;
-	});
+	}
 
-});
+	function closeMenu() {
+		$('.menu-open-left').removeClass('menu-open-left');
+		$('.button-open-left').removeClass('button-open-left');
+		$('.menu-open-right').removeClass('menu-open-right');
+		$('.button-open-right').removeClass('button-open-right');
+		$('.active').removeClass('active');
+	}
+
+	function openMenu() {
+		if(menuAlignment == 'left') {
+			$('#' + menuId).addClass('menu-open-left');
+			$('#' + groupId).addClass('button-open-left');
+		} else {
+			$('#' + menuId).addClass('menu-open-right');
+			$('#' + groupId).addClass('button-open-right');
+		}
+	}
+
+	return false;
+
+}
