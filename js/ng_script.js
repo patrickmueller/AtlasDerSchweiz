91945142fab2ec9dc021352dd752343df8a902e9
Patrick Müller <patrick@MrsWormwood.local>
2013-10-10 22:08:45 +0200

    Topicmenu implemented

--- js/ng_script.js
+++ js/ng_script.js
@@ -15,10 +15,10 @@ angular.module('ngView', ['ngRoute']).config(function($routeProvider, $locationP
   });
 
   $routeProvider.when('/', {
-    templateUrl: 'home.html',
-    //templateUrl: 'map.html',
-    controller: MainCntl,
-    //controller: MapCntl
+    //templateUrl: 'home.html',
+    templateUrl: 'map.html',
+    //controller: MainCntl,
+    controller: MapCntl
   });
  
   // configure html5 to get links working on jsfiddle
@@ -54,12 +54,64 @@ function ProjectCntl($scope, $routeParams) {
   navigationControl();
 }
  
-function MapCntl($scope, $routeParams) {
+function MapCntl($scope, $routeParams, $http, $templateCache) {
 
   initialize();
   navigationControl();
 
   $scope.image = '';
+
+  $scope.topics = null;
+  $scope.topicList = null;
+  $scope.topicTitle = 'Themen';
+  $scope.topicIndex2 = null;
+  $scope.topicIndexCrumbs = new Array();;
+  
+  $.getJSON('http://atlas.dev/json/topics.json', function(json) {
+      console.log(json);
+      $scope.$apply(function(){
+          $scope.topics = json;
+          $scope.topicList = json;
+      });
+  });
+
+  $scope.changeTopic = function(index) {
+
+    $scope.topicIndexCrumbs.push(index);
+    var i = $scope.topicIndexCrumbs;
+
+    if(i.length == 1) {
+      $scope.topicList = $scope.topics[i[0]]['children'];
+      $scope.topicTitle  = $scope.topics[i[0]]['name'];
+    }
+
+    if(i.length == 2) {
+      $scope.topicList = $scope.topics[i[0]]['children'][i[1]]['children'];
+      $scope.topicTitle  = $scope.topics[i[0]]['children'][i[1]]['name'];
+    }
+
+    if(i.length == 3) {
+      $scope.topicList = $scope.topics[i[0]]['children'][i[1]]['children'][i[2]]['children'];
+      $scope.topicTitle  = $scope.topics[i[0]]['children'][i[1]]['children'][i[2]]['title_long'];
+    }
+
+/*
+
+    console.log(index);
+
+    if(index2 != '') {
+      $scope.topicList = $scope.topics[index2]['children'][index]['children'];
+      $scope.topicTitle  = $scope.topics[index2]['children'][index]['name'];
+    } else {
+      $scope.topicList = $scope.topics[index]['children'];
+      $scope.topicTitle  = $scope.topics[index]['name'];
+      $scope.topicIndex2 = index;
+    }
+    */
+
+  }
+
+
 
   $scope.search = function() {
     $scope.image = 'img/map_suche.png';
